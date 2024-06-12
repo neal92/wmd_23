5cc5d99161c8ee32f76c1410838408ce0158e2eb
neal92 <93376019+neal92@users.noreply.github.com>
2024-06-12 15:32:15 +0200

    Update vue_form.php

--- vues/vue_form.php
+++ vues/vue_form.php
@@ -1,11 +1,23 @@
-<h3> Connexion Utilisateur </h3>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Connexion Utilisateur</h5>
 
-<form method="post" action="index.php?page=3">
-    <label for="email">Email:</label>
-    <input type="email" name="email" id="email" required>
-    <br>
-    <label for="mdp">Mot de passe:</label>
-    <input type="password" name="mdp" id="mdp" required>
-    <br>
-    <input type="submit" name="Connecter" value="Se Connecter">
-</form>
+                    <form method="post" action="index.php?page=3">
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Adresse Email</label>
+                            <input type="email" name="email" id="email" class="form-control" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="mdp" class="form-label">Mot de passe</label>
+                            <input type="password" name="mdp" id="mdp" class="form-control" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary" value="Se Connecter" name="Connecter">Se Connecter</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
